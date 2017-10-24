2a9fdb5e7fdb378c01722a0449fd470966155716
ellismiranda <Ellis@MacBook-Pro.local>
2017-10-24 00:48:27 -0400

    [Add] new getArtistId method that returns the SID for the artist returned by getArtistByName. also an error function because why not.

--- models/artists.js
+++ models/artists.js
@@ -1,11 +1,38 @@
 const superagent = require('superagent');
 
-// make a check for whether or not anything is returned based on this Promise
+// Returns all information about artists found given the name
 function getArtistByName(name, access_token) {
   return superagent.get('https://api.spotify.com/v1/search')
                    .query({q: name, type: 'artist'})
                    .set('Authorization', 'Bearer ' + access_token);
 
+}
+
+// Returns the SID of the first artist in the list of artists gotten from getArtistByName
+// This will return a specific flag if there are no artists returned.
+function getArtistId(name, access_token) {
+  getArtistByName(name, access_token).then((res, err) => {
+    if (err) error(err);
+    else {
+      artists = res.body.artists.items;
+      if (artists[0]) {
+        return artists[0].id;
+      } else {
+        return -1;
+      }
+
+    }
+  })
+}
+
+// Using a SID, gets the top songs of the artist
+function getArtistTopSongs(name, access_token) {
+  artistId = getArtistId(name, access_token);
+}
+
+//because why not
+function error(err) {
+  console.log('ERROR\n', err);
 }
 
 module.exports = {

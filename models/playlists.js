01aadea556ee8d0c205e99a7240df5c576e5b3b6
ellismiranda <Ellis@MacBook-Pro.local>
2017-10-24 00:23:13 -0400

    [Add] New playlists.js, includes getUserPlaylists - get user's playlists by their id

---
+++ models/playlists.js
@@ -0,0 +1,10 @@
+const superagent = require('superagent');
+
+function getUserPlaylists(userId, access_token) {
+  return superagent.get(`https://api.spotify.com/v1/users/${userId}/playlists`)
+                   .set('Authorization', 'Bearer ' + access_token);
+}
+
+module.exports = {
+  getPlaylists
+}
